43925ea6b972f75507ac0ae89dd8f8bd413c70fa
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-01-10 17:06:42 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,6 +4,8 @@
  * _strlen_recursion - Returns the length of a string
  * @s: the string to input
  *
+ * This function calculates the length of the string using recursion.
+ * It returns the length of the string
  */
 int _strlen_recursion(char *s)
 {
