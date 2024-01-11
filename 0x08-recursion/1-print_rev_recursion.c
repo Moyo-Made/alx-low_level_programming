7a62ce9259b50c7b65aeb8907197e663420dba11
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-01-10 16:19:03 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  _print_rev_recursion - Prints a string in reverse
+ *  @s: The string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s == NULL)
+	{
+		return;
+	}
+
+	int length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
