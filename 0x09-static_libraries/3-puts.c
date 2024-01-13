82b9a7692d8d646a098c6bad6896f68d59670f7a
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-01-12 20:23:41 -0800

    Updated file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * _puts - to put the str
  * @str: the string
  */
-void _puts(char *str);
+void _puts(char *str)
+{
+	puts(str);
+}
