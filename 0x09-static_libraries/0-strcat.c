2ab6e3e9e63fcb24230f34f0981d02ee70a39b65
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-01-12 18:20:43 -0800

    Created the static library libmy.a containing all the functions listed

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat- function that concactenates two strings
+ * @dest: first entered value
+ * @src: second entered value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * _puts - to put the str
+ * @str: the string
+ */
+void _puts(char *str);
