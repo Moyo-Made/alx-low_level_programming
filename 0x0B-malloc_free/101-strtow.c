2e8e7a0602939426c657a45688b0ce8b5081a7b5
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-16 14:09:41 +0100

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,7 +7,7 @@
 */
 char **strtow(char *str)
 {
-	int word_count, i, j, word_index, start, word_length;
+	int word_count, i, j, k, l;
 	char **words;
 	
 	if (str == NULL || *str == '\0')
@@ -17,7 +17,7 @@ char **strtow(char *str)
 	word_count = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0' || str[i + 1] == '.'))
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0' ||  ispunct(str[i + 1])))
 		{
 			word_count++;
 		}
@@ -31,38 +31,36 @@ char **strtow(char *str)
 		{
 			return (NULL);
 		}
-		word_index = 0;
-		start = 0;
+		k = 0;
 		for (i = 0; str[i] != '\0'; i++)
 		{
 			if (str[i] != ' ')
 			{
 				if (i == 0 || str[i - 1] == ' ')
 				{
-					start = i;
-				}
-			}
-			else if (i > 0 && str[i - 1] != ' ')
-			{
-				word_length = i - start;
-				words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-				if (words[word_index] == NULL)
-				{
-					for (j = 0; j < word_index; j++)
+					j = i;
+					while (str[i] != ' ' && str[i] != '\0' && !ispunct(str[i]))
 					{
-						free(words[j]);
+						i++;
 					}
-					free(words);
-					return (NULL);
+					words[k] = (char *)malloc((i - j + 1) * sizeof(char));
+					if (words[k] == NULL)
+					{
+						for (j = 0; j < k; j++)
+						{
+							free(words[j]);
+						}
+						free(words);
+						return (NULL);
+					}
+					for (l = 0; j < i; j++, l++)
+					{
+						words[k][l] = str[j];
+					}
+					words[k][i -j] = '\0';
+					k++;
 				}
-				for (j = 0; j < word_length; j++)
-				{
-					words[word_index][j] = str[start + j];
-				}
-				words[word_index][word_length] = '\0';
-				word_index++;
 			}
-	}
-	words[word_index] = NULL;
-	return (words);
+			words[k] = NULL;
+			return (words);
 }
