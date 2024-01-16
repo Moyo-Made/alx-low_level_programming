0c3851f566b064b57e2ae08463f782ea81118fc6
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-16 19:58:59 +0100

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,6 +9,7 @@ int count_word(char *s)
 {
 	int flag, c, w;
 	flag = 0;
+
 	w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
@@ -16,6 +17,7 @@ int count_word(char *s)
 		if (s[c] == ' ')
 		{
 			flag = 0;
+
 		}
 		else if (flag == 0){
 			flag = 1;
