50d81f81f81fdc6e64752eb26cb51a5da4062db5
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-16 13:49:46 +0100

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,6 +9,7 @@ char **strtow(char *str)
 {
 	int word_count, i, j, word_index, start, word_length;
 	char **words;
+	
 	if (str == NULL || *str == '\0')
 	{
 		return (NULL);
@@ -16,7 +17,7 @@ char **strtow(char *str)
 	word_count = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0' || str[i + 1] == '.'))
 		{
 			word_count++;
 		}
