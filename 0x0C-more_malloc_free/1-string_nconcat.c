dfd0a571340693932acb36dd5342d42925a536ab
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-19 00:02:36 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,31 +15,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 
 	if (s1 == NULL)
+	{
 		s1 == "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 == "";
-
+	}
 	while (s1[len_s1] != '\0')
+	{
 		len_s1++;
-
+	}
 	while (s2[len_s2] != '\0')
+	{
 		len_s2++;
-
+	}
 	if (n >= len_s2)
+	{
 		n = len_s2;
-
-		concat = malloc(sizeof(char) * (len_s1 + n + 1));
+	}
+	concat = malloc(sizeof(char) * (len_s1 + n + 1));
 
 		if (concat == NULL)
+		{
 			return (NULL);
-
+		}
 		for (i = 0; i < len_s1; i++)
+		{
 			concat[i] = s1[i];
-
+		}
 		for (j = 0; j < n; j++)
+		{
 			concat[i + j] = s2[j];
-
+		}
 		concat[i + j] = '\0';
 
 		return (concat);
