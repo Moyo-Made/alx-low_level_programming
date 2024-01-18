3543462570e6385e0052828870e2e3572cf277b3
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-18 23:20:48 +0100

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: integer to allocate
+ * Return: a function that allocates memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		_putchar('e');
+		_putchar('\n');
+		exit(98);
+	}
+	return (ptr);
+}
