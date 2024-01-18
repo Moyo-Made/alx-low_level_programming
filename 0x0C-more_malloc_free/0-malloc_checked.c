de1425d6215619aab4c6ccccb0a9954c2bdc9962
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-19 00:18:27 +0100

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr; 
+	
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
-		_putchar('e');
-		_putchar('\n');
 		exit(98);
 	}
 	return (ptr);
