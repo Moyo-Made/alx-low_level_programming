f52957081b3a841e8d3ef46e725f05dd04a17a75
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-23 19:23:06 +0100

    Writes a program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  * 
  * Return: Always 0(Success)
 */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-		return (0);
+	return (0);
 }
