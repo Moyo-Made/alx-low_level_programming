a71e4f04d429d58bd518802cfcaa97fc182d6573
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-23 22:14:08 +0100

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_STRUCT_H
-#define DOG_STRUCT_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - structure representing information about a dog
@@ -14,4 +14,12 @@ struct dog
 	char *owner;
 };
 
-#endif /*DOG_STRUCT_H*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct  dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
