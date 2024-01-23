6dcb3a55e66a38b1ee19d2c8f89ea2ef96eab96d
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-23 23:53:47 +0100

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,7 @@ int _strlen(char *s)
 	{
 		i++;
 	}
+
 	return (i);
 }
 /**
@@ -28,7 +29,6 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
-	len = 0;
 	while (src[len] != '\0')
 	{
 		len++;
@@ -53,18 +53,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
+
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
@@ -72,9 +74,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
+
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
-	dog-> = age;
+	dog->age = age;
 
 	return (dog);
 }
