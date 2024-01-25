0a01d270c8641aa323890b3bfbc779eda8a4804b
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-25 02:16:49 +0100

    a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - Prints each character of a name using a
- * given function pointer
+ * print_name - Prints name using pointer to function
  * @name: The string containing the name
- * @f: function pointer to a functin that takes char*
- * as an argument
+ * @f: pointer to function
+ * Return: Nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
-	for (; *name; name++)
-	{
-		f(*name);
-	}
-	f('\n');
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
