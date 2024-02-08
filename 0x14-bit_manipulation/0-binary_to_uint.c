11f1eaa6061609c557b7e17ee2d57d52de0f4af0
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-08 22:11:11 +0100

    Code: converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,12 @@ unsigned int binary_to_uint(const char *b)
 	num = 0;
 	if (!b)
 		return (0);
-	for (a = 0; b[a] != "\0"; a++)
+	for (a = 0; b[a] != '\0'; a++)
 	{
-		if (b[a] != "0" && b[a] != "1")
+		if (b[a] != '0' && b[a] != '1')
 			return (0);
 	}
-	for (a = 0; b[a] != "\0"; a++)
+	for (a = 0; b[a] != '\0'; a++)
 	{
 		num <<= 1;
 		if (b[a] == '1')
