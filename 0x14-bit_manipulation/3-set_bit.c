ce50e9d2c65ae28328244102baff3ad3bfeb5a58
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-08 23:25:30 +0100

    Code: sets the value of bits to 1 at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,6 +4,8 @@
  * set_bit - func sets the value of bits to 1 at a given index
  * @n: number to set
  * @index: index at which to set bit
+ *
+ * Return: value of bit set
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
